5c08a0107f7880f67d081af223d6eabd3035a186
Antonio <acarelli@studenti.apuliadigitalmaker.it>
2024-11-29 09:44:55 +0100

    backend definitivo

---
+++ handler/macchinari.py
@@ -0,0 +1,38 @@
+from fastapi import APIRouter, status, HTTPException
+from modello.basemodel import Macchinari
+from utilità.utils import machinery_collection, plant_collection
+from bson.objectid import ObjectId
+from utilità.utils import toString
+
+router = APIRouter()
+
+@router.get("/macchinari")
+async def get_machinery():
+    macchinari = list(machinery_collection.find())
+    for macchinario in macchinari:
+        macchinario["_id"] = toString(macchinario["_id"])
+    return macchinari
+
+@router.get("/impianti/{id}/macchinari")
+async def get_machinery_by_plant(id: str):
+    macchinari = list(machinery_collection.find({"plant_id": id}))
+    for macchinario in macchinari:
+        macchinario["_id"] = toString(macchinario["_id"])
+    return macchinari
+                                                 
+@router.get("/macchinari/{id}")
+async def get_machinery(id: str):
+    macchinario = machinery_collection.find_one({"_id": ObjectId(id)})
+    if macchinario:
+        macchinario["_id"] = toString(macchinario["_id"])
+        return macchinario
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Macchinario non trovato")
+
+@router.post("/impianti/{id}/macchinari")
+async def create_machinery(id: str, macchinario: Macchinari):
+    macchinario.plant_id = id
+    response = machinery_collection.insert_one(macchinario.model_dump())
+    plant_collection.update_one({"_id": ObjectId(id)}, 
+                                {"$push": {"machinery": str(response.inserted_id)}})
+    return {"_id": toString(response.inserted_id)}
+
---
+++ main.py
@@ -0,0 +1,29 @@
+from fastapi import FastAPI
+from handler.autenticazione import router as autenticazione_router
+from handler.impianti import router as impianti_router
+from handler.macchinari import router as macchinari_router
+from fastapi.middleware.cors import CORSMiddleware
+
+
+app = FastAPI(  
+
+    openapi_url="/documentation/json",
+
+    docs_url="/documentation",
+
+    redoc_url=None
+
+)
+
+origins = ["*"]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+app.include_router(autenticazione_router)
+app.include_router(impianti_router)
+app.include_router(macchinari_router)
